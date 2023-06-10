3d93f321034b10fa724a29f9dea54da4d26e73f0
SilRoxo <silroxobeltran@gmail.com>
2023-06-10 14:32:43 -0300

    criado sistema de apresentar dados do tutor atual

--- js/EntradaFormulario.js
+++ js/EntradaFormulario.js
@@ -1,18 +1,3 @@
 // JavaScript source code
-function gerarTutor() {
-    localStorage.clickcount++
-    var novoTutor = new Object();
-    Object.keys(dados.tutor).forEach((item) => novoTutor[item]=document.getElementById(item).value);
-    
-    return JSON.stringify(novoTutor);
-}
-function salvarFormularioCadastroTutor() {
-    var cad = gerarTutor();
-    localStorage.setItem('Tutor_' + localStorage.clickcount, cad);
-    gerarFormAnimal(dados.animal,'Tutor_' + localStorage.clickcount);
-}
-function gerarFormAnimal(nomeFormulario){
-   // criarForm(dados.animal,nomeFormulario);
 
-}
 //function criarAtributo()
