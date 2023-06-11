75e1c34a3350c0da4f0fb30e8cfaf86e2c6c9813
SilRoxo <silroxobeltran@gmail.com>
2023-06-11 11:47:27 -0300

    funcção de editar e apagar dados no formulario

--- js/ManipularTabela.js
+++ js/ManipularTabela.js
@@ -1,24 +1,30 @@
 function checarArmazenamento(){
-    console.log(localStorage.clickcount);
     if (localStorage.hasOwnProperty('clickcount')==false){
         localStorage.clickcount = 0;
     }
-    return document.write(`<h1>Temos ${localStorage.clickcount} tutores no armanazenamento local!</h1>`)
+    if (localStorage.hasOwnProperty('totalTutores')==false){
+        localStorage.totalTutores = 0;
+    }
+    return document.write(`<h1>Temos ${localStorage.totalTutores} tutores no armanazenamento local!</h1>`)
 }
 function criarTabela(){
-    document.write('<table>');
+    document.write('<form id="selecionarDados"><table>');
     document.write('<tr>');
     colunas=[];
     Object.keys(dados.tutor).forEach((item) =>criarTabheader(item));
     Object.keys(dados.tutor).forEach((item) =>colunas.push(item));
     document.write('</tr>');
-    for(var i = 1; i<=localStorage.clickcount; i++){
-        var tutor = JSON.parse(localStorage.getItem('Tutor_' + i));
+    for (var chave in localStorage){
+        if (chave.substring(0,chave.indexOf('_'))=='Tutor'){
+        var tutor = JSON.parse(localStorage.getItem(chave));
         document.write('<tr>');
         criarLinha(tutor,colunas);
+        document.write(`<td><input type="radio" name="Escolhatutor" value=${chave} id="selecionar_${chave}"></td>`);
+        
         document.write('</tr>');
-     }
-     document.write('</table>');
+     }}
+     document.write(`</table><button form="selecionarDados"  onClick ="mudarDados()" name="bt_MudarTutor">Mudar dados</button></form>`);
+     document.write(`</table><button form="selecionarDados"  onClick ="apagarTutor()" name="bt_ApagarTutor">Apagar dados</button></form>`);
 }
 function criarLinha(obj,colunas){
         for (var item of colunas) {
@@ -32,3 +38,4 @@ function criarLinha(obj,colunas){
 function criarTabheader(coluna){
     document.write(`<th> ${coluna}</th>`)
 }
+
--- js/formulario.js
+++ js/formulario.js
@@ -1,52 +1,97 @@
 ////////////////CRIA FORMULÁRIO
 function criarForm(obj,nomeFormulario){
+    AtualizarQuantidadeTutores()
     var elemento = document.getElementById(`div_${nomeFormulario}`);
     var conteudo = `<form id=${nomeFormulario}><ul>`
     for (var item in obj) {
         if (obj.hasOwnProperty(item)) {
             conteudo+=`<li>
                             <label for="${nomeFormulario}_${item}">${obj[item].label}:</label>
-                            <input type=${obj[item].tipo} id="${nomeFormulario}_${item}"`
+                            <input type=${obj[item].tipo} id=${nomeFormulario}_${item}`; 
+                            var itemObj = obj[item]
+                            Object.keys(itemObj).forEach((j) => conteudo += ' '+ novoAtributo(j,itemObj[j]) + ' ');
             if(obj[item].hasOwnProperty('obrigatorio')){
                 conteudo+=` required `;
             }
-            conteudo+='/></li>';
+            conteudo+=' /></li>';
         }
     }
     conteudo+=`<li>
-        <input type="submit" form=${nomeFormulario}  onClick ="salvarFormulario${nomeFormulario}()" id="Salvar_${nomeFormulario}">      
+        <button type="button" form=${nomeFormulario}  onClick ="salvarFormulario${nomeFormulario}()" id="Salvar_${nomeFormulario}">Salvar novo Tutor
+        <button type="submit" form=${nomeFormulario}  onClick ="editar${nomeFormulario}()" id="Editar_${nomeFormulario}">Editar Dados
                 </li>
                 </ul>
             </form> </div>`;
     elemento.innerHTML+=conteudo;
 }
+function novoAtributo(obj,item){
+    return `${obj} = ${item}`;
+}
+function AtualizarQuantidadeTutores(){
+    var totalTutores = 0;
+    for (var chave in localStorage){
+        if (chave.substring(0,chave.indexOf('_'))=='Tutor'){
+            totalTutores++;
+        }
+    
+    }
+    localStorage.totalTutores=totalTutores;
+}
 ///////////////////GERA TUTOR
 function gerarTutor() {
   //  localStorage.console = "0";
     var novoTutor = new Object();
-
+    AtualizarQuantidadeTutores();
     Object.keys(dados.tutor).forEach((item) => novoTutor[item]=document.getElementById(`CadastroTutor_${item}`).value);
     return JSON.stringify(novoTutor);
 }
 function salvarTutor(nomeForm) {
- localStorage.clickcount++
+    if (validarDados(nomeForm)){;
+        localStorage.clickcount++
+        var cad = gerarTutor();
+        localStorage.setItem('Tutor_' + localStorage.clickcount, cad);
+        localStorage.tutorAtual = 'Tutor_' + localStorage.clickcount;
+    }
+}
+function editarCadastroTutor(){
     var cad = gerarTutor();
-    localStorage.setItem('Tutor_' + localStorage.clickcount, cad);
-    localStorage.tutorAtual = 'Tutor_' + localStorage.clickcount;
-  //  gerarFormAnimal(nomeForm);
+    localStorage.setItem(localStorage.tutorAtual,cad)
 }
 function gerarFormAnimal(nomeFormulario){
     console.log(nomeFormulario);
-   // criarForm(dados.animal,nomeFormulario);
 
 }
-/////////////////////CHECA FORMULÁRIO E INSERE CONTEUDO COMO PLACEHOLDER
+/////////////////////CHECA FORMULÁRIO E INSERE CONTEUDO COMO PLACEHOLDER ou valor
 function popularForm(ReferenciaDado,formId){
+    var comoInserir ='value';
     var dadoCarregado = JSON.parse(localStorage.getItem(ReferenciaDado));
     for (var item in dadoCarregado){
-        console.log(item);
+
         var elemento= document.getElementById(`${formId}_${item}`);
-        elemento.setAttribute('value',dadoCarregado[item]);
-        console.log(dadoCarregado)
+        elemento.setAttribute(comoInserir,dadoCarregado[item]);
+        elemento.setAttribute("placeHolder",dadoCarregado[item]);
     }
+}
+function mudarDados(){
+
+    localStorage.tutorAtual=document.querySelector('input[name="Escolhatutor"]:checked').value;
+}
+function apagarTutor(){
+    var tutorRemovido=document.querySelector('input[name="Escolhatutor"]:checked').value;
+    localStorage.removeItem(tutorRemovido);
+    localStorage.tutorAtual=0;
+    AtualizarQuantidadeTutores();
+}
+function validarDados(nomeForm,form){
+    for (var chave in dados[form]){
+        console.log(form);
+    }
+    mostrarLightbox();
+    return false;
+}
+function mostrarLightbox(){
+    document.getElementById("lightbox").style.display = 'block';
+}
+function fecharLightbox(){
+    document.getElementById("lightbox").style.display = "none";
 }
