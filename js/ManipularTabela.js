57fb1ce4508238d202ca653264ce16cabec681b9
SilRoxo <silroxobeltran@gmail.com>
2023-06-12 00:32:40 -0300

    implementado busca

--- js/ManipularTabela.js
+++ js/ManipularTabela.js
@@ -2,40 +2,159 @@ function checarArmazenamento(){
     if (localStorage.hasOwnProperty('clickcount')==false){
         localStorage.clickcount = 0;
     }
-    if (localStorage.hasOwnProperty('totalTutores')==false){
-        localStorage.totalTutores = 0;
+    if (localStorage.hasOwnProperty('totaltutor')==false){
+        localStorage.totaltutor = 0;
     }
-    return document.write(`<h1>Temos ${localStorage.totalTutores} tutores no armanazenamento local!</h1>`)
+    if (localStorage.hasOwnProperty('dadosAExibir')==false){
+        localStorage.dadosAExibir = "todos";
+    }
+    var titulo = document.createElement("h1")
+    titulo.textContent = `Temos ${localStorage.totaltutor} tutore(s) no armanazenamento local!`;
+    var elemento = document.getElementById("anuncio")
+    elemento.appendChild(titulo);
 }
-function criarTabela(){
-    document.write('<form id="selecionarDados"><table>');
-    document.write('<tr>');
+function criarTabela(nomeForm){
+    var ondeInsere = document.getElementById('tabelaDados');
+    var formulario = document.createElement("form");
+    formulario.id = "selecionarDados"
+    var tabela = document.createElement("table");
+    formulario.appendChild(tabela);
+    ondeInsere.appendChild(formulario);
     colunas=[];
-    Object.keys(dados.tutor).forEach((item) =>criarTabheader(item));
-    Object.keys(dados.tutor).forEach((item) =>colunas.push(item));
-    document.write('</tr>');
-    for (var chave in localStorage){
-        if (chave.substring(0,chave.indexOf('_'))=='Tutor'){
-        var tutor = JSON.parse(localStorage.getItem(chave));
-        document.write('<tr>');
-        criarLinha(tutor,colunas);
-        document.write(`<td><input type="radio" name="Escolhatutor" value=${chave} id="selecionar_${chave}"></td>`);
+    //// aqui cria o cabeçalho
+    var primeiraLinha = document.createElement("tr");
+    tabela.appendChild(primeiraLinha)
+    Object.keys(dados[nomeForm]).forEach((item) =>primeiraLinha.appendChild(criarTabheader(item)));
+
+    Object.keys(dados[nomeForm]).forEach((item) =>colunas.push(item));
+   var quaisExibir = new Object();
+   console.log(localStorage.dadosAExibir)
+    if(!(localStorage.dadosAExibir=='todos')){
         
-        document.write('</tr>');
-     }}
-     document.write(`</table><button form="selecionarDados"  onClick ="mudarDados()" name="bt_MudarTutor">Mudar dados</button></form>`);
-     document.write(`</table><button form="selecionarDados"  onClick ="apagarTutor()" name="bt_ApagarTutor">Apagar dados</button></form>`);
-}
-function criarLinha(obj,colunas){
-        for (var item of colunas) {
-            if (obj.hasOwnProperty(item)) {
-            document.write(`<td> ${obj[item]}</td>`);
-        }else{
-                document.write('<td></td>')
+        quaisExibir = JSON.parse(localStorage.dadosAExibir);
+    }
+     for (var chave in localStorage){
+        if (chave.substring(0,chave.indexOf('_'))==nomeForm){
+            var verseExibe =false;
+            if(quaisExibir.hasOwnProperty(chave)){
+                if(quaisExibir[chave]){
+                    verseExibe = true;
+                }
+            }
+            if((localStorage.dadosAExibir=='todos')||(verseExibe)){
+                console.log("foi o dados a exibir:"+localStorage.dadosAExibir)
+                console.log("foi o verseExibe:"+verseExibe); 
+                var dadosForm = JSON.parse(localStorage.getItem(chave));
+            //cria linha
+                var linha = tabela.insertRow();
+                linha.id = "linha"+chave;
+            //cria celulas
+                for (var item in dadosForm){
+                    var celula = linha.insertCell()
+                    if(dadosForm.hasOwnProperty(item)){
+                        celula.textContent=dadosForm[item];
+                    }
+                    
+                }
+                
+            criarLinha(dadosForm,colunas);
+            var radioItem = document.createElement("input");
+            radioItem.type = "radio"
+            radioItem.value = chave;
+            radioItem.id = "selecionar_"+chave;
+            radioItem.name = "Escolha"+nomeForm
+            var celInput = linha.insertCell();
+            celInput.append(radioItem);
+       
+            }
         }
     }
+    if(localStorage[`${nomeForm}Atual`]!=0){
+        if(quaisExibir.hasOwnProperty(localStorage[`${nomeForm}Atual`])){
+            if(quaisExibir[localStorage[`${nomeForm}Atual`]]){
+                document.getElementById("selecionar_"+localStorage[nomeForm+'Atual']).setAttribute("checked","true");
+            }
+        }
+       
+    }
+    var apagarDados = document.createElement("button");
+    apagarDados.setAttribute("onclick",`apagarDado('${nomeForm}')`);
+    apagarDados.setAttribute("type","button")
+    apagarDados.name = `bt_Apagar${nomeForm}`;
+    apagarDados.textContent = "Apagar Dados"
+    apagarDados.form = "selecionar_Dados";
+    var mudarDados = document.createElement("button");
+    mudarDados.textContent = "MudarDados";
+    mudarDados.setAttribute("onclick","mudarDados()");
+    mudarDados.name = "bt_MudarTutor";
+    mudarDados.form = "selecionar_Dados";
+    ondeInsere.appendChild(apagarDados);
+    ondeInsere.appendChild(mudarDados);
+        
+    
+}
+function criarLinha(obj,colunas){
+        var linha
+        for (var item of colunas) {
+            if (obj.hasOwnProperty(item)) {
+        }
+    }
+    return linha
 }
 function criarTabheader(coluna){
-    document.write(`<th> ${coluna}</th>`)
+    var elemento = document.createElement("th");
+    elemento.textContent=coluna
+    elemento.name = coluna;
+    return elemento;
+}
+/////buscador
+function criarBuscador(nomeForm){
+    
+    var ondeInserir = document.getElementById('tabelaDados');
+     
+    var paragrafo = document.createElement("div");
+    var buscador = document.createElement("form");
+    buscador.id = "Buscador";
+    var etiqueta = document.createElement('label');
+    etiqueta.textContent="Faça sua busca:";
+    var campo = document.createElement("input");
+    campo.id = "input_buscador"
+    var botao = document.createElement("button");
+    botao.id="bt_buscar";
+    botao.textContent = "Buscar"
+    botao.setAttribute("onclick",`buscarDado('${nomeForm}')`)
+    
+    tabela = document.getElementById('selecionarDados');
+    ondeInserir.insertBefore(paragrafo,tabela);
+    paragrafo.appendChild(buscador);
+    buscador.appendChild(etiqueta);
+    buscador.appendChild(campo);
+    buscador.append(botao);
+}
+function buscarDado(nomeForm)
+{
+
+    console.log("dado a ser buscado: "+document.getElementById("input_buscador").value);
+    var dadoBuscado = document.getElementById("input_buscador").value;
+    var dadoBuscadomin = dadoBuscado.toLowerCase()
+    var dadosAExibir = new Object
+    for (var chave in localStorage){
+        if (chave.substring(0,chave.indexOf('_'))==nomeForm){
+            var dadosForm = JSON.parse(localStorage.getItem(chave));
+            console.log(dadosForm.nome)
+            var nomeMinus = dadosForm.nome.toLowerCase();
+            if (nomeMinus.includes(dadoBuscado))
+            {
+                dadosAExibir[chave] = true;
+            }else{
+                dadosAExibir[chave] = false;
+            }
+            
+        }
+       // console.log(localStorage[`${nomeForm}_1`]);
+     //   console.log(nomeForm);
+    }
+    localStorage.dadosAExibir = JSON.stringify(dadosAExibir);
+    console.log(dadosAExibir);
 }
 
--- js/formulario.js
+++ js/formulario.js
@@ -1,6 +1,6 @@
 ////////////////CRIA FORMULÁRIO
 function criarForm(obj,nomeFormulario){
-    AtualizarQuantidadeTutores()
+    AtualizarDados(nomeFormulario);
     var elemento = document.getElementById(`div_${nomeFormulario}`);
     var conteudo = `<form id=${nomeFormulario}><ul>`
     for (var item in obj) {
@@ -10,55 +10,63 @@ function criarForm(obj,nomeFormulario){
                             <input type=${obj[item].tipo} id=${nomeFormulario}_${item}`; 
                             var itemObj = obj[item]
                             Object.keys(itemObj).forEach((j) => conteudo += ' '+ novoAtributo(j,itemObj[j]) + ' ');
-            if(obj[item].hasOwnProperty('obrigatorio')){
-                conteudo+=` required `;
+            conteudo+=` />`;
+            if(obj[item].hasOwnProperty('validador')){
+                conteudo +=`<p class='mensagem' id='mensagem${nomeFormulario}_${item}'>O ${obj[item].label} deve ${obj[item].validador.mensagem}</p>`
             }
-            conteudo+=' /></li>';
+            conteudo+=`</li>`;
         }
     }
     conteudo+=`<li>
-        <button type="button" form=${nomeFormulario}  onClick ="salvarForm(${nomeFormulario})" id="Form_${nomeFormulario}">Salvar novo Tutor
-        <button type="submit" form=${nomeFormulario}  onClick ="editarForm(${nomeFormulario})" id="Editar_${nomeFormulario}">Editar Dados
+        <button type="button" form=${nomeFormulario}  onClick ="salvarForm('${nomeFormulario}')" id="Form_${nomeFormulario}">Salvar novo ${nomeFormulario}
+        <button type="button" form=${nomeFormulario}  onClick ="editarForm('${nomeFormulario}')" id="Editar_${nomeFormulario}">Editar dados de ${nomeFormulario}
                 </li>
                 </ul>
             </form> </div>`;
     elemento.innerHTML+=conteudo;
 }
 function novoAtributo(obj,item){
-    return `${obj} = ${item}`;
+    if(obj!="validador"){
+        return `${obj} = ${item}`;
+    }
 }
-function AtualizarQuantidadeTutores(){
-    var totalTutores = 0;
+function AtualizarDados(form){
+    var totalDados = 0;
     for (var chave in localStorage){
-        if (chave.substring(0,chave.indexOf('_'))=='Tutor'){
-            totalTutores++;
+        if (chave.substring(0,chave.indexOf('_'))==form){
+            totalDados++;
         }
     
     }
-    localStorage.totalTutores=totalTutores;
+    localStorage[`total${form}`]=totalDados;
 }
-///////////////////GERA TUTOR
-function gerarTutor() {
-  //  localStorage.console = "0";
-    var novoTutor = new Object();
-    AtualizarQuantidadeTutores();
-    Object.keys(dados.tutor).forEach((item) => novoTutor[item]=document.getElementById(`tutor_${item}`).value);
-    return JSON.stringify(novoTutor);
+///////////////////GERA DADO
+function gerarDado(nomeForm) {
+    var novoDado = new Object();
+    AtualizarDados(nomeForm);
+    Object.keys(dados.tutor).forEach((item) => novoDado[item]=document.getElementById(`${nomeForm}_${item}`).value);
+    localStorage.dadosAExibir = 'todos';
+    return JSON.stringify(novoDado);
 }
 function salvarForm(nomeForm) {
-    if (validarDados(nomeForm)){;
+    if (validarDados(nomeForm)){
         localStorage.clickcount++;
-        if  (nomeForm=='tutor'){
-            var cad = gerarTutor();
-            localStorage.setItem('Tutor_' + localStorage.clickcount, cad);
-            localStorage.tutorAtual = 'Tutor_' + localStorage.clickcount;
-        }
+        var cad = gerarDado(nomeForm);
+            localStorage.setItem(nomeForm+'_' + localStorage.clickcount, cad);
+            localStorage.tutorAtual = nomeForm+ '_' + localStorage.clickcount;
+        location.reload()
+    }else{
+        mostrarLightbox();
     }
 }
 function editarForm(form){
-    if(form = 'tutor'){
-        var cad = gerarTutor();
+    if (validarDados(form)){
+        var cad = gerarDado(form);
         localStorage.setItem(localStorage.tutorAtual,cad);
+        location.reload()
+    }
+    else{
+        mostrarLightbox();
     }
 }
 function gerarFormAnimal(nomeFormulario){
@@ -67,34 +75,52 @@ function gerarFormAnimal(nomeFormulario){
 }
 /////////////////////CHECA FORMULÁRIO E INSERE CONTEUDO COMO PLACEHOLDER ou valor
 function popularForm(ReferenciaDado,formId){
-    var comoInserir ='value';
     var dadoCarregado = JSON.parse(localStorage.getItem(ReferenciaDado));
     for (var item in dadoCarregado){
 
         var elemento= document.getElementById(`${formId}_${item}`);
-        elemento.setAttribute(comoInserir,dadoCarregado[item]);
+        elemento.setAttribute("value",dadoCarregado[item]);
         elemento.setAttribute("placeHolder",dadoCarregado[item]);
     }
 }
 function mudarDados(){
 
     localStorage.tutorAtual=document.querySelector('input[name="Escolhatutor"]:checked').value;
+    location.reload();
 }
-function apagarTutor(){
-    var tutorRemovido=document.querySelector('input[name="Escolhatutor"]:checked').value;
-    localStorage.removeItem(tutorRemovido);
-    localStorage.tutorAtual=0;
-    AtualizarQuantidadeTutores();
+function apagarDado(form){
+    console.log(form);
+    var dadoRemovido=document.querySelector(`input[name="Escolha${form}"]:checked`).value;
+    localStorage.removeItem(dadoRemovido);
+    localStorage[`${form}Atual`]=0;
+    AtualizarDados(form);
+    location.reload()
 }
+//////////////////////VALIDAÇÃO DOS DADOS E CONTROLE DO LIGHTBOX
 function validarDados(form){
+    var todosValidados = true
     for (var chave in dados[form]){
-        console.log(form);
+        if(dados[form][chave].hasOwnProperty('validador'))
+        {
+            var dadoInserido = document.getElementById(`${form}_${chave}`).value;
+             var stringPadrao = dados[form][chave].validador.regra;
+             var validador = RegExp(stringPadrao);
+             if(!validador.test(dadoInserido)){
+                
+                console.log("A expressão: "+dadoInserido)
+                console.log('não passou: '+validador)
+                document.getElementById('mensagem'+form+'_'+chave).style.display = 'block'  ;
+
+                return false
+             }
+             document.getElementById('mensagem'+form+'_'+chave).style.display = 'none'
+        }
+        
     }
-    mostrarLightbox();
-    return false;
+    return true;
 }
 function mostrarLightbox(){
-    document.getElementById("lightbox").style.display = 'block';
+    document.getElementById("lightbox").style.display = 'flex';
 }
 function fecharLightbox(){
     document.getElementById("lightbox").style.display = "none";
